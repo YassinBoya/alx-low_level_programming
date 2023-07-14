c433d260d539ec86d37041767728db52acdac2ff
YassinBoya <hondayassin5@gmail.com>
2023-07-14 14:38:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
 * main - A program that prints a line to the standars error
-* Return 1 (Success)
+* Return always 0 (Success)
 */
 int main(void)
 {
-puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
