22feb1afff3ba9b890666af88a13f45ed1a2c42c
YassinBoya <hondayassin5@gmail.com>
2023-07-14 14:42:01 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-/*
-* main - A programme that prints a line with printf function
-* Return: 0 (success)
-*/
+/**
+ * main - A programme that prints a line with printf function
+ * Return: 0 (success)
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
