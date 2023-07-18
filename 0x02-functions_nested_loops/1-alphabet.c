0f8bb1dbc6e97bda5a9c4ad62311c011c573f268
YassinBoya <hondayassin5@gmail.com>
2023-07-18 16:20:25 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,10 @@
 */
 void print_alphabet(void)
 {
-		int i;
-		
-		for (i = 97; i <= 122; i++)
-		{
-			putchar( i );
-		}
-		putchar('\n');
-	
-	return(0);
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
