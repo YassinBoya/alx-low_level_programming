a1c3789278645dc7ee9f07838ac81d7957caa3c8
YassinBoya <hondayassin5@gmail.com>
2023-07-18 18:37:08 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,15 @@ int print_last_digit(int n)
 {
 	int ld;
 
+	if (n < 0)
+	{
+		n = -n;
+	}
 	ld = n % 10;
 	if (ld < 0)
 	{
-		putchar(-ld + 48);
-		return (-ld);
+		ld = -ld;
 	}
-	else
-	{
-		putchar(ld + 48);
-		return (ld);
-	}
+	_putchar(ld + '0');
+	return(ld);
 }
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+/**
+ * jack_bauer -  a function that prints every minute of the day of Jack Bauer
+ * Return: always 0
+ */
+void jack_bauer(void)
+{
