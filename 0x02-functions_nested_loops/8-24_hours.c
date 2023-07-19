62d415b70572143333ce5c5995acbee6c00fff40
YassinBoya <hondayassin5@gmail.com>
2023-07-19 16:40:21 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,3 +5,17 @@
  */
 void jack_bauer(void)
 {
+	int min, hr;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+	for (min =0; min < 60; min++)
+	{
+	_putchar((hr / 10) + 48);
+_putchar((hr % 10) + 48);
+_putchar(':');
+_putchar((min / 10) + 48);
+_putchar((min % 10) + 48);
+	}
+	}
+}
