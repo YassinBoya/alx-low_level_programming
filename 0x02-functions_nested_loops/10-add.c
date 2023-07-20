9b14b9d075565a8584f23b47a439681a198a1681
YassinBoya <hondayassin5@gmail.com>
2023-07-19 17:44:35 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * add - a fucntion that's prints the add o ftwo numbers
- * @a @b: 2 number passed as parametres of the function
+ * @a: first number to add passed as parametre
+ * @b: second number passed as parametre
  * Return: always 0
  */
 int add(int a, int b)
 {
 	int sum = a + b;
+
 	return (sum);
 }
