3851fc1d5d2e5569a8185dee696346c6424a8967
YassinBoya <hondayassin5@gmail.com>
2023-07-20 08:26:21 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,38 +6,43 @@
  */
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-	n = -n;
-    }
-    if (n / 10)
-    {
-	    print_number(n / 10);
-    }
-
-    _putchar((n % 10) + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar((n % 10) + '0');
 }
 void print_to_98(int n)
 {
 	int i;
-
-    if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            print_number(i);
-            if (i != 98) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    } else {
-        for (i = n; i >= 98; i--) {
-            print_number(i);
-            if (i != 98) {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    _putchar('\n');
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_number(i);
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
