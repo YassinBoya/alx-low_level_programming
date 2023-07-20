07bc553ecbf2e37b3c96da76db67be8558c51a9f
YassinBoya <hondayassin5@gmail.com>
2023-07-20 08:17:15 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,34 +4,30 @@
  * @n: number integer gived as parametre
  * Return: always 0
  */
+void print_number(int n) {
+    if (n < 0) {
+        _putchar('-');
+        n = -n;
+    }
+
+    if (n / 10)
+        print_number(n / 10);
+
+    _putchar((n % 10) + '0');
+}
+
 void print_to_98(int n) {
-    int i;
-
     if (n <= 98) {
-        for (i = n; i <= 98; i++) {
-            if (i / 100)
-                _putchar((i / 100) + '0');
-
-            if (i / 10)
-                _putchar(((i % 100) / 10) + '0');
-            
-            _putchar((i % 10) + '0');
-
+        for (int i = n; i <= 98; i++) {
+            print_number(i);
             if (i != 98) {
                 _putchar(',');
                 _putchar(' ');
             }
         }
     } else {
-        for (i = n; i >= 98; i--) {
-            if (i / 100)
-                _putchar((i / 100) + '0');
-
-            if (i / 10)
-                _putchar(((i % 100) / 10) + '0');
-            
-            _putchar((i % 10) + '0');
-
+        for (int i = n; i >= 98; i--) {
+            print_number(i);
             if (i != 98) {
                 _putchar(',');
                 _putchar(' ');
