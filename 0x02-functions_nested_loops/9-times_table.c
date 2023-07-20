6bcaaa6e0b589c7d539b5dfaa64141ecb0abe0f9
YassinBoya <hondayassin5@gmail.com>
2023-07-19 17:37:10 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,9 +24,9 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((product / 10) + 0);
+				_putchar((product / 10) + '0');
 			}
-			_putchar((product % 10) + 0);
+			_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
 	}
