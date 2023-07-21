5295dafeffa887efc597e39470cfe3e8739bdef7
YassinBoya <hondayassin5@gmail.com>
2023-07-21 12:37:09 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,12 +6,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = '0'; j <= "14"; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j);
+			if ( j > 9)
+			{
+				_putchar('0' + (j /10));
+			}
+			_putchar('0' + (j % 10));
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
