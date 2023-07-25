b91829f7e6ff3796f970039c4af3af7e5d2ff023
YassinBoya <hondayassin5@gmail.com>
2023-07-24 18:10:36 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string
+ * @str: apointer to a sting
+ */
+void puts_half(char *str)
+{
+	int i, length = 0;
+
+	while (str[length] != '\0')
+		length++;
+	for (i = (length / 2); i < 10; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
