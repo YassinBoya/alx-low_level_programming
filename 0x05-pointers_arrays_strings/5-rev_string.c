30907595c91057bd7b41b13fc660144544089fb2
YassinBoya <hondayassin5@gmail.com>
2023-07-24 19:41:24 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,12 +7,12 @@ void rev_string(char *s)
 {
 	int length = 0;
 	int i;
+	char temp;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
-	char temp;
 	for (i = 0; i < length / 2; i++)
 	{
 		temp = s[i];
