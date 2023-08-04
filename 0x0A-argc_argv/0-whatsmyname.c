31b6ba6dfa936e6e5de87cff599f98a3363b19ac
YassinBoya <hondayassin5@gmail.com>
2023-08-04 07:46:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - the entry point
  * @argc: the length of arguments in argv
  * @argv: an array of arguments
  * Return: always 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
