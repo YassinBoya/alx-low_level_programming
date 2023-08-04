fb0068448cec6a1cd98c2ce5d980b65b4bdb89d7
YassinBoya <hondayassin5@gmail.com>
2023-08-04 06:35:48 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the entry point
+ * @argc: the length of arguments in argv
+ * @argv: an array of arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	putchar("%s\n", argv[argc - argc]);
+	return (0);
+}
