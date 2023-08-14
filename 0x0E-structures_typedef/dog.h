57b7fd0cd9a62b1bde9a6d408cf7a45000e2d815
YassinBoya <hondayassin5@gmail.com>
2023-08-14 09:30:53 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * dog - a structur that holds the name and the age and the owen of a dog
+ * @name: the first member
+ * @age: the second member
+ * @owner: the third member
+ */
+ 
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
