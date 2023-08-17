cde4ed76671f9b7f40c322b8fba1b60d09c18d04
YassinBoya <hondayassin5@gmail.com>
2023-08-17 11:42:30 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,8 +2,48 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
+ * print_char - a function handles printing a character argument
+ * @separator: the separator to be printed before the character
+ * @args: the list of arguments
+ */
+void print_char(char separator, va_list args)
+{
+	printf("%c%c", separator, va_arg(args, int));
+}
+/**
+ * print_int - a function handles printing an integer argument
+ * @separator: the separator to be printed before the character
+ * @args: the list of arguments
+ */
+void print_int(char separator, va_list args)
+{
+	printf("%c%d", separator, va_arg(args, int));
+}
+/**
+ * print_float - a function handles printing a float argument
+ * @separator: the separator to be printed before the character
+ * @args: the list of arguments
+ */
+void print_float(char separator, va_list args)
+{
+	printf("%c%f", separator, va_arg(args, double));
+}
+/**
+ * print_string - afunction handles printing a string argument
+ * @separator: the separator to be printed before the character
+ * @args: the list of arguments
+ */
+void print_string(char separator, va_list args)
+{
+	char *s_arg = va_arg(args, char *);
+
+	if (s_arg == NULL)
+		s_arg = "(nil)";
+	printf("%c%s", separator, s_arg);
+}
+/**
  * print_all - a function that prints any type of variables
- * @format: the format string specifying the types of arguments.
+ *@format: the format string specifying the types of arguments
  */
 void print_all(const char * const format, ...)
 {
@@ -15,29 +55,32 @@ void print_all(const char * const format, ...)
 
 	while (*fmt)
 	{
-		if (*fmt == 'c')
-		{
-			printf("%s%c", separator, va_arg(args, int));
-		}
-		else if (*fmt == 'i')
-		{
-			printf("%s%d", separator, va_arg(args, int));
-		}
-		else if (*fmt == 'f')
-		{
-			printf("%s%f", separator, va_arg(args, double));
-		}
-		else if (*fmt == 's')
-		{
-			char *s_arg = va_arg(args, char *);
+		char current_format = *fmt;
 
-			if (s_arg == NULL)
-				s_arg = "(nil)";
-			printf("%s%s", separator, s_arg);
+		if (*(fmt + 1) != '\0')
+			separator = ", ";
+
+		switch (current_format)
+		{
+			case 'c':
+				print_char(*separator, args);
+				break;
+			case 'i':
+				print_int(*separator, args);
+				break;
+			case 'f':
+				print_float(*separator, args);
+				break;
+			case 's':
+				print_string(*separator, args);
+				break;
+			default:
+				break;
 		}
-		separator = ", ";
+
 		fmt++;
 	}
+
 	va_end(args);
 	printf("\n");
 }
