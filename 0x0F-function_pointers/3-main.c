bd06dd1406afc3e42a3305622b1782ba5209182f
YassinBoya <hondayassin5@gmail.com>
2023-08-16 19:59:02 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		printf("Error\n"), exit (98);
+		printf("Error\n"), exit(98);
 	}
 
 	num1 = atoi(argv[1]);
@@ -25,11 +25,11 @@ int main(int argc, char **argv)
 
 	if (operation == NULL)
 	{
-		printf("Error\n"), exit (99);
+		printf("Error\n"), exit(99);
 	}
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
-		printf("Error\n"), exit (100);
+		printf("Error\n"), exit(100);
 	}
 	printf("%d\n", operation(num1, num2));
 	return (0);
