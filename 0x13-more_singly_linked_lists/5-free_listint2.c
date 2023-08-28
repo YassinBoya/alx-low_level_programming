0a8e746dc021fd0983357698e1657aae2add75cc
YassinBoya <hondayassin5@gmail.com>
2023-08-28 14:09:05 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 {
     listint_t *current = *head, *next;
     if (!current)
-	    return (NULL);
+	    return;
     while (current)
     {
         next = current->next;
