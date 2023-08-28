b83aa032ce49c36796deafac83567e373b410053
YassinBoya <hondayassin5@gmail.com>
2023-08-28 12:21:08 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * listint_len - afuction that retun the number of elements in the list 
+ * listint_len -  a function that returns the number
+ * of elements in a linked listint_t list.
  * @h: the list
+ * Return: the number of nodes in the list
  */
 size_t listint_len(const listint_t *h)
 {
