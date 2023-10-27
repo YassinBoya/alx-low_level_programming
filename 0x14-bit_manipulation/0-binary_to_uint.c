2465ebb8208a12a0f90bf0d4aa4069f42e462148
YassinBoya <hondayassin5@gmail.com>
2023-10-26 18:35:43 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 
 /**
  * binary_to_uint - converts a binary number to an
@@ -9,29 +11,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int len, i, power, sum = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	ui = 0;
+	len = strlen(b);
 
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i = 0; i < len; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
-
-		if (b[len] & 1)
+		if (b[i] == '1')
 		{
-			ui += base_two;
+			power = len - 1 - i;
+			sum += 1 << power;
 		}
 	}
 
-	return (ui);
+	return sum;
 }
+
