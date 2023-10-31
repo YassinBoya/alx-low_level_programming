15ef8e5da36290a8c87f99771ae5d75951a0d5a5
YassinBoya <hondayassin5@gmail.com>
2023-10-30 18:54:57 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,15 +11,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes_read;
-	char buf[READ_BUF_SIZE * 8];
+	char buf[READ_BUF_SIZE];
 
 	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	bytes_read = read(fd, &buf[0], letters);
-	bytes_read = write(STDOUT_FILENO, &buf[0], bytes_read);
+	bytes_read = read(fd, buf, letters);
+	if(bytes_read > 0)
+	write(STDOUT_FILENO, buf, bytes_read);
 	close(fd);
 	return (bytes_read);
 }
