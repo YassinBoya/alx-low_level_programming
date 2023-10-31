b9a66d5138de273b34c005cafd0d768edc6aa2c3
YassinBoya <hondayassin5@gmail.com>
2023-10-30 19:51:40 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		while (text_content[len] != '\0';)
 			len++;
 	}
-	fd = open(filename, O_WRONLY | OAPPEND)
+	fd = open(filename, O_WRONLY | O_APPEND)
 	if (fd == -1 || write(fd, text_content, len) == -1)
 		return (-1);
 	close(fd);
