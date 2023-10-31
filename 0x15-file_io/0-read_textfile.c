1681463123fac03c6a11d9bfff822214fa412cab
YassinBoya <hondayassin5@gmail.com>
2023-10-30 18:43:44 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,22 +7,23 @@
  * @letters: is the number of letters it should read and print
  * Return: the actual number of letters it could read and print
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
+ssize_t read_textfile(const char *filename, size_t letters)
+{
 	int fd;
 	ssize_t bytes_read = 0;
 	char buf[READ_BUF_SIZE];
 
 	if (!filename || letters == 0)
-		return 0;
+		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
+		return (0);
 
 	bytes_read = read(fd, buf, letters);
 	if (bytes_read > 0)
 		write(STDOUT_FILENO, buf, bytes_read);
 
 	close(fd);
-	return bytes_read;
+	return (bytes_read);
 }
