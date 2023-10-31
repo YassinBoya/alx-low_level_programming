86e8daf2929568d20444c94384174d6272c317f7
YassinBoya <hondayassin5@gmail.com>
2023-10-30 19:00:02 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes_read;
-	char buf[READ_BUF_SIZE];
+	char buf[READ_BUF_SIZE * 8];
 
 	if (!filename || !letters)
 		return (0);
