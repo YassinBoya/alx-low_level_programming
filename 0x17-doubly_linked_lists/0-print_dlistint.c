206767909c8ef69e2dac7bca5b1dc6c5b9d4dcc5
Yassine <hondayassin5@gmail.com>
2023-12-07 18:36:21 +0100

    commit of task 4

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -24,20 +24,3 @@ counter++;
 }
 return (counter);
 }
-
-/**
-* free_dlistint - Frees a doubly linked list.
-* @head: A pointer to the head of the list to be freed.
-*/
-
-void free_dlistint(dlistint_t *head)
-{
-dlistint_t *temp;
-
-while (head != NULL)
-{
-temp = head;
-head = head->next;
-free(temp);
-}
-}
