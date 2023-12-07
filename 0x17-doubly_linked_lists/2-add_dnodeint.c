8765cd9d00e005432aeb27476b018c9ac723d9c7
Yassine <hondayassin5@gmail.com>
2023-12-07 17:10:45 +0100

    commit of task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,11 +12,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *temp_list = malloc(sizeof(dlistint_t));
 
-if (temp_list == NULL)
-{
-return (NULL);
-}
-
 temp_list->prev = NULL;
 temp_list->n = n;
 temp_list->next = NULL;
