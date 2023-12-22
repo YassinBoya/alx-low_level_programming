8273796eba991f8b5d93079b3d5d1a1e1058ae64
Yassine <hondayassin5@gmail.com>
2023-12-22 21:49:20 +0100

    commit of task 2

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_djb2 - Compute the hash value using the djb2 algorithm.
+ * @str: The input string (unsigned character array) to be hashed.
+ * Return: The computed hash value as an unsigned long int.
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
