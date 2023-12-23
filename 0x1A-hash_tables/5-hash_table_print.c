71d71bc9036f1fa0f07893f00bb7430cb50ed226
Yassine <hondayassin5@gmail.com>
2023-12-23 02:34:10 +0100

    commit of task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,25 +8,25 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
-    int flag = 0;
-    hash_node_t *temp;
+unsigned long int i;
+int flag = 0;
+hash_node_t *temp;
 
-    if (ht == NULL)
-        return;
+if (ht == NULL)
+return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        temp = ht->array[i];
-        while (temp != NULL)
-        {
-            if (flag == 1)
-                printf(", ");
-            printf("'%s': '%s'", temp->key, temp->value);
-            temp = temp->next;
-            flag = 1;
-        }
-    }
-    printf("}\n");
+printf("{");
+for (i = 0; i < ht->size; i++)
+{
+temp = ht->array[i];
+while (temp != NULL)
+{
+if (flag == 1)
+printf(", ");
+printf("'%s': '%s'", temp->key, temp->value);
+temp = temp->next;
+flag = 1;
+}
+}
+printf("}\n");
 }
